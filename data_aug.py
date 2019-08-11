0741044efc7e93e59b009bcfe375f4919c0a6f72
LordkTk <49175881+LordkTk@users.noreply.github.com>
2019-08-11 15:04:15 +0800

    Update data_aug.py

--- data_aug.py
+++ data_aug.py
@@ -4,7 +4,7 @@ Created on Tue Jul  2 14:43:42 2019
 
 @author: cfd_Liu
 """
-
+'This data_augmentation file is from wizyoungs repo and is slightly modified for my own data format'
 import cv2
 import numpy as np
 import random
@@ -29,30 +29,36 @@ def load_aug_img(path, batchList, imgSize, imgInfo_train, is_training=True):
     imgbatch = np.concatenate(imgbatch, axis=0)
     return imgbatch, imgInfo
 def data_augmentation(imgSrc, bboxSrc, w, h, imgSize):
-    img = random_brightness(imgSrc)
-    
-    img, bbox = random_flip(img, bboxSrc)
-    
-    
-    
+    # random color jittering
+    # NOTE: applying color distort may lead to bad performance sometimes
+    boxes = bboxSrc.copy()
+    img = random_color_distort(imgSrc)
+
+    # random expansion with prob 0.5
     if np.random.uniform(0, 1) > 0.5:
-        img, bbox = random_expand(img, bbox, 3)
-        
+        img, boxes = random_expand(img, boxes, 4)
+
+    # random cropping
     h, w, _ = img.shape
-    if np.random.uniform(0, 1) > 0.5:
-        img, bbox = resize_with_bbox(img, bbox, imgSize, imgSize)
-        
-    h, w, _ = img.shape
-    bbox, crop = random_crop_with_constraints(bbox, (w, h))
+    boxes, crop = random_crop_with_constraints(boxes, (w, h))
     x0, y0, w, h = crop
     img = img[y0: y0+h, x0: x0+w]
+
+    # resize with random interpolation
+    h, w, _ = img.shape
+    interp = np.random.randint(0, 5)
+    img, boxes = resize_with_bbox(img, boxes, imgSize, imgSize, interp=interp, letterbox=False)
+
+    # random horizontal flip
+    h, w, _ = img.shape
+    img, boxes = random_flip(img, boxes, px=0.5)
     
-    if not val_bbox(bbox):
+    if not val_bbox(boxes):
         img = imgSrc
-        bbox = bboxSrc
+        boxes = bboxSrc
     
     h, w = img.shape[:2]
-    return img, bbox, w, h
+    return img, boxes, w, h
 def val_bbox(bbox):
     w = bbox[:, 2] - bbox[:, 0]
     h = bbox[:, 3] - bbox[:, 1]
@@ -73,105 +79,6 @@ def get_imgInfo(bbox, w, h):
         Info.append(tmp)
     Info.append([w, h])
     return Info
-def letterbox_resize(img, new_width, new_height):
-    '''
-    Letterbox resize. keep the original aspect ratio in the resized image.
-    '''
-    ori_height, ori_width = img.shape[:2]
-
-    resize_ratio = min(new_width / ori_width, new_height / ori_height)
-
-    resize_w = int(resize_ratio * ori_width)
-    resize_h = int(resize_ratio * ori_height)
-
-    img = cv2.resize(img, (resize_w, resize_h))
-    image_padded = np.full((new_height, new_width, 3), 128, np.uint8)
-
-    dw = int((new_width - resize_w) / 2)
-    dh = int((new_height - resize_h) / 2)
-
-    image_padded[dh: resize_h + dh, dw: resize_w + dw, :] = img
-
-    return image_padded, resize_ratio, dw, dh
-
-def resize_with_bbox(img, bbox, new_width, new_height):
-    '''
-    Resize the image and correct the bbox accordingly.
-    '''
-
-    image_padded, resize_ratio, dw, dh = letterbox_resize(img, new_width, new_height)
-
-    # xmin, xmax
-    bbox[:, [0, 2]] = bbox[:, [0, 2]] * resize_ratio + dw
-    # ymin, ymax
-    bbox[:, [1, 3]] = bbox[:, [1, 3]] * resize_ratio + dh
-    return image_padded, bbox
-
-def random_flip(img, bbox, px=0.5, py=0):
-    '''
-    Randomly flip the image and correct the bbox.
-    param:
-    px:
-        the probability of horizontal flip
-    py:
-        the probability of vertical flip
-    '''
-    bbox = bbox.copy()
-    height, width = img.shape[:2]
-    if np.random.uniform(0, 1) < px:
-        img = cv2.flip(img, 1)
-        xmax = width - bbox[:, 0]
-        xmin = width - bbox[:, 2]
-        bbox[:, 0] = xmin
-        bbox[:, 2] = xmax
-
-    if np.random.uniform(0, 1) < py:
-        img = cv2.flip(img, 0)
-        ymax = height - bbox[:, 1]
-        ymin = height - bbox[:, 3]
-        bbox[:, 1] = ymin
-        bbox[:, 3] = ymax
-    return img, bbox
-
-def random_brightness(img, brightness_delta=32, p=0.5):
-    if np.random.uniform(0, 1) > p:
-        img = img.astype(np.float32)
-        brightness_delta = int(np.random.uniform(-brightness_delta, brightness_delta))
-        img = img + brightness_delta
-    return np.clip(img, 0, 255)
-
-def random_expand(img, bbox, max_ratio=4, fill=0, keep_ratio=True):
-    '''
-    Random expand original image with borders, this is identical to placing
-    the original image on a larger canvas.
-    param:
-    max_ratio :
-        Maximum ratio of the output image on both direction(vertical and horizontal)
-    fill :
-        The value(s) for padded borders.
-    keep_ratio : bool
-        If `True`, will keep output image the same aspect ratio as input.
-    '''
-    h, w, c = img.shape
-    ratio_x = random.uniform(1, max_ratio)
-    if keep_ratio:
-        ratio_y = ratio_x
-    else:
-        ratio_y = random.uniform(1, max_ratio)
-
-    oh, ow = int(h * ratio_y), int(w * ratio_x)
-    off_y = random.randint(0, oh - h)
-    off_x = random.randint(0, ow - w)
-
-    dst = np.full(shape=(oh, ow, c), fill_value=fill, dtype=img.dtype)
-
-    dst[off_y:off_y + h, off_x:off_x + w, :] = img
-
-    # correct bbox
-    bbox[:, :2] += (off_x, off_y)
-    bbox[:, 2:4] += (off_x, off_y)
-
-    return dst, bbox
 
 def bbox_crop(bbox, crop_box=None, allow_outside_center=True):
     """Crop bounding boxes according to slice area.
@@ -309,7 +216,7 @@ def random_crop_with_constraints(bbox, size, min_scale=0.3, max_scale=1,
             (0.5, None),
             (0.7, None),
             (0.9, None),
-#            (None, 1),
+            (None, 1),
         )
 
     w, h = size
@@ -353,11 +260,173 @@ def random_crop_with_constraints(bbox, size, min_scale=0.3, max_scale=1,
         return new_bbox, new_crop
     return bbox, (0, 0, w, h)
 
+
+def random_color_distort(img, brightness_delta=32, hue_vari=18, sat_vari=0.5, val_vari=0.5):
+    '''
+    randomly distort image color. Adjust brightness, hue, saturation, value.
+    param:
+        img: a BGR uint8 format OpenCV image. HWC format.
+    '''
+
+    def random_hue(img_hsv, hue_vari, p=0.5):
+        if np.random.uniform(0, 1) > p:
+            hue_delta = np.random.randint(-hue_vari, hue_vari)
+            img_hsv[:, :, 0] = (img_hsv[:, :, 0] + hue_delta) % 180
+        return img_hsv
+
+    def random_saturation(img_hsv, sat_vari, p=0.5):
+        if np.random.uniform(0, 1) > p:
+            sat_mult = 1 + np.random.uniform(-sat_vari, sat_vari)
+            img_hsv[:, :, 1] *= sat_mult
+        return img_hsv
+
+    def random_value(img_hsv, val_vari, p=0.5):
+        if np.random.uniform(0, 1) > p:
+            val_mult = 1 + np.random.uniform(-val_vari, val_vari)
+            img_hsv[:, :, 2] *= val_mult
+        return img_hsv
+
+    def random_brightness(img, brightness_delta, p=0.5):
+        if np.random.uniform(0, 1) > p:
+            img = img.astype(np.float32)
+            brightness_delta = int(np.random.uniform(-brightness_delta, brightness_delta))
+            img = img + brightness_delta
+        return np.clip(img, 0, 255)
+
+    # brightness
+    img = random_brightness(img, brightness_delta)
+    img = img.astype(np.uint8)
+
+    # color jitter
+    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV).astype(np.float32)
+
+    if np.random.randint(0, 2):
+        img_hsv = random_value(img_hsv, val_vari)
+        img_hsv = random_saturation(img_hsv, sat_vari)
+        img_hsv = random_hue(img_hsv, hue_vari)
+    else:
+        img_hsv = random_saturation(img_hsv, sat_vari)
+        img_hsv = random_hue(img_hsv, hue_vari)
+        img_hsv = random_value(img_hsv, val_vari)
+
+    img_hsv = np.clip(img_hsv, 0, 255)
+    img = cv2.cvtColor(img_hsv.astype(np.uint8), cv2.COLOR_HSV2BGR)
+
+    return img
+
+
+def letterbox_resize(img, new_width, new_height, interp=0):
+    '''
+    Letterbox resize. keep the original aspect ratio in the resized image.
+    '''
+    ori_height, ori_width = img.shape[:2]
+
+    resize_ratio = min(new_width / ori_width, new_height / ori_height)
+
+    resize_w = int(resize_ratio * ori_width)
+    resize_h = int(resize_ratio * ori_height)
+
+    img = cv2.resize(img, (resize_w, resize_h), interpolation=interp)
+    image_padded = np.full((new_height, new_width, 3), 128, np.uint8)
+
+    dw = int((new_width - resize_w) / 2)
+    dh = int((new_height - resize_h) / 2)
+
+    image_padded[dh: resize_h + dh, dw: resize_w + dw, :] = img
+
+    return image_padded, resize_ratio, dw, dh
+
+
+def resize_with_bbox(img, bbox, new_width, new_height, interp=0, letterbox=False):
+    '''
+    Resize the image and correct the bbox accordingly.
+    '''
+
+    if letterbox:
+        image_padded, resize_ratio, dw, dh = letterbox_resize(img, new_width, new_height, interp)
+
+        # xmin, xmax
+        bbox[:, [0, 2]] = bbox[:, [0, 2]] * resize_ratio + dw
+        # ymin, ymax
+        bbox[:, [1, 3]] = bbox[:, [1, 3]] * resize_ratio + dh
+
+        return image_padded, bbox
+    else:
+        ori_height, ori_width = img.shape[:2]
+
+        img = cv2.resize(img, (new_width, new_height), interpolation=interp)
+
+        # xmin, xmax
+        bbox[:, [0, 2]] = bbox[:, [0, 2]] / ori_width * new_width
+        # ymin, ymax
+        bbox[:, [1, 3]] = bbox[:, [1, 3]] / ori_height * new_height
+
+        return img, bbox
+
+
+def random_flip(img, bbox, px=0, py=0):
+    '''
+    Randomly flip the image and correct the bbox.
+    param:
+    px:
+        the probability of horizontal flip
+    py:
+        the probability of vertical flip
+    '''
+    height, width = img.shape[:2]
+    if np.random.uniform(0, 1) < px:
+        img = cv2.flip(img, 1)
+        xmax = width - bbox[:, 0]
+        xmin = width - bbox[:, 2]
+        bbox[:, 0] = xmin
+        bbox[:, 2] = xmax
+
+    if np.random.uniform(0, 1) < py:
+        img = cv2.flip(img, 0)
+        ymax = height - bbox[:, 1]
+        ymin = height - bbox[:, 3]
+        bbox[:, 1] = ymin
+        bbox[:, 3] = ymax
+    return img, bbox
+
+
+def random_expand(img, bbox, max_ratio=4, fill=0, keep_ratio=True):
+    '''
+    Random expand original image with borders, this is identical to placing
+    the original image on a larger canvas.
+    param:
+    max_ratio :
+        Maximum ratio of the output image on both direction(vertical and horizontal)
+    fill :
+        The value(s) for padded borders.
+    keep_ratio : bool
+        If `True`, will keep output image the same aspect ratio as input.
+    '''
+    h, w, c = img.shape
+    ratio_x = random.uniform(1, max_ratio)
+    if keep_ratio:
+        ratio_y = ratio_x
+    else:
+        ratio_y = random.uniform(1, max_ratio)
+
+    oh, ow = int(h * ratio_y), int(w * ratio_x)
+    off_y = random.randint(0, oh - h)
+    off_x = random.randint(0, ow - w)
+
+    dst = np.full(shape=(oh, ow, c), fill_value=fill, dtype=img.dtype)
+
+    dst[off_y:off_y + h, off_x:off_x + w, :] = img
+
+    # correct bbox
+    bbox[:, :2] += (off_x, off_y)
+    bbox[:, 2:4] += (off_x, off_y)
+
+    return dst, bbox
 if __name__ == '__main__':
-    path_train = np.load('./data/path_train.npy').item()[1]
+    path_train = np.load('./data/path_test.npy').item()[1]
     imgSize_train = 416
-    imgInfo_train = np.load('./data/dataset_train.npy').item()
-    trainList = np.load('./data/trainList.npy').item()[1]
+    imgInfo_train = np.load('./data/dataset_test.npy').item()
+    trainList = np.load('./data/testList.npy').item()[1]
     batchList = trainList[:5]
     
     Info_tmp = {}
